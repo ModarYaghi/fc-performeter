4d022ee624f559380a303534c95914f0822e440a
ModarYaghi <mfyaghi81@gmail.com>
2024-02-15 12:34:45 +0300

    compile raw data

--- path_management.py
+++ path_management.py
@@ -27,65 +27,65 @@ processed_data = os.path.join(root, data, processed, data_dir)
 unvfvt23 = os.path.join(root, data, processed, "unvfvt23")
 # --------------------------------------------------------------
 scr_path, scr_sheet = (
-    os.path.join(processed_data, "ps" + "scr" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "00_" + "ps" + "scr" + "_" + data_dir + ".csv"),
     "Scr",
 )
 
 int_path, int_sheet = (
-    os.path.join(processed_data, "ps" + "int" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "01_" + "ps" + "int" + "_" + data_dir + ".csv"),
     "Int",
 )
 
-gc_path, gc_sheet = (os.path.join(processed_data, "ps" + "gc" + "_" + data_dir + ".csv"), "GC")
+gc_path, gc_sheet = (os.path.join(processed_data, "02_" + "ps" + "gc" + "_" + data_dir + ".csv"), "GC")
 
-ic_path, ic_sheet = (os.path.join(processed_data, "ps" + "ic" + "_" + data_dir + ".csv"), "IC")
+ic_path, ic_sheet = (os.path.join(processed_data, "03_" + "ps" + "ic" + "_" + data_dir + ".csv"), "IC")
 
 psfua_path, psfua_sheet = (
-    os.path.join(processed_data, "psfua" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "04_" + "psfua" + "_" + data_dir + ".csv"),
     "FUA",
 )
 
 pei_path, pei_sheet = (
-    os.path.join(processed_data, "ps" + "pei" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "05_" + "ps" + "pei" + "_" + data_dir + ".csv"),
     "PEI",
 )
 
-td_path, td_sheet = (os.path.join(processed_data, "ps" + "td" + "_" + data_dir + ".csv"), "TD")
+td_path, td_sheet = (os.path.join(processed_data, "06_" + "ps" + "td" + "_" + data_dir + ".csv"), "TD")
 
 cws_path, cws_sheet = (
-    os.path.join(processed_data, "ps" + "cws" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "07_" + "ps" + "cws" + "_" + data_dir + ".csv"),
     "CWS",
 )
 
 trw_path, trw_sheet = (
-    os.path.join(processed_data, "ps" + "trw" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "08_" + "ps" + "trw" + "_" + data_dir + ".csv"),
     "TRW",
 )
 
-aw_path, aw_sheet = (os.path.join(processed_data, "ps" + "aw" + "_" + data_dir + ".csv"), "AW")
+aw_path, aw_sheet = (os.path.join(processed_data, "09_" + "ps" + "aw" + "_" + data_dir + ".csv"), "AW")
 
 psfs_path, psfs_sheet = (
-    os.path.join(processed_data, "pt" + "psfs" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "10_" + "pt" + "psfs" + "_" + data_dir + ".csv"),
     "PSFS",
 )
 
 ptint_path, ptint_sheet = (
-    os.path.join(processed_data, "pt" + "ptint" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "11_" + "pt" + "ptint" + "_" + data_dir + ".csv"),
     "PT Int",
 )
 
 gpt_path, gpt_sheet = (
-    os.path.join(processed_data, "pt" + "gpt" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "12_" + "pt" + "gpt" + "_" + data_dir + ".csv"),
     "GPT",
 )
 
 ipt_path, ipt_sheet = (
-    os.path.join(processed_data, "pt" + "ipt" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "13_" + "pt" + "ipt" + "_" + data_dir + ".csv"),
     "IPT",
 )
 
 ptfua_path, ptfua_sheet = (
-    os.path.join(processed_data, "ptfua" + "_" + data_dir + ".csv"),
+    os.path.join(processed_data, "14_" + "ptfua" + "_" + data_dir + ".csv"),
     "FUA",
 )
 
