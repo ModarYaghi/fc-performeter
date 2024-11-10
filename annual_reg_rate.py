f832aa6634b308f4f1b28645edbb5b4d33bd04d3
Modar Yaghi <mfyaghi81@gmail.com>
2024-11-11 02:24:17 +0300

    translation completed

--- annual_reg_rate.py
+++ annual_reg_rate.py
@@ -1,16 +1,17 @@
 # from src.all_in_one import *
 import pandas as pd
 
-
 scr = pd.read_csv("data/processed/0624/00_psscr_0624.csv")
-scr['sc_s1'] = pd.to_datetime(scr['sc_s1'])
-scr['sc_s1'] = scr['sc_s1'].dt.year
+scr["sc_s1"] = pd.to_datetime(scr["sc_s1"])
+scr["sc_s1"] = scr["sc_s1"].dt.year
 
 
-annual_registration_counts = scr.groupby('sc_s1').size().reset_index(name='count')
-total_registrations = annual_registration_counts['count'].sum()
+annual_registration_counts = scr.groupby("sc_s1").size().reset_index(name="count")
+total_registrations = annual_registration_counts["count"].sum()
 
-annual_registration_counts['percentage'] = (annual_registration_counts['count'] / total_registrations) * 100
+annual_registration_counts["percentage"] = (
+    annual_registration_counts["count"] / total_registrations
+) * 100
 
 
 print(annual_registration_counts)
--- deepl_translator/irm_final_eval_trans.py
+++ deepl_translator/irm_final_eval_trans.py
@@ -1,28 +1,107 @@
 # %%
+"""Importing necessary libraries and source code"""
 import pandas as pd
 from irm_final_eval_sources import excel_file_path, open_questions_rename_mapping
 
 from deepl_translator import translate_text, translate_dataframe_column
 
 # %%
-open_questions = pd.read_excel(excel_file_path, sheet_name='open_questions')
+open_questions = pd.read_excel(excel_file_path, sheet_name="open_questions")
 open_questions.rename(columns=open_questions_rename_mapping, inplace=True)
 
 # %%
 open_questions.head(1)
 # %%
-gossip_exclusion_exp_index = open_questions.columns.get_loc('gossip_exclusion_exp')
-open_questions.insert(
-    gossip_exclusion_exp_index + 1,
-    'gossip_exc_exp_en',
-    open_questions['gossip_exclusion_exp'].apply(
-        translate_text
-    )
-    )
-
-# %%
-translate_dataframe_column(open_questions, 'gossip_exclusion_exp', 'gossip_exc_exp_en', translate_text)
-# %%
+open_questions = translate_dataframe_column(
+    open_questions, "gossip_exclusion_exp", "gossip_exc_exp_en", translate_text
+)
 open_questions.head(1)
 # %%
-translate_dataframe_column(open_questions, 'exp_1', 'exp_en_1', translate_text)
+open_questions = translate_dataframe_column(
+    open_questions, "exp_1", "exp_en_1", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "exp_2", "exp_en_2", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "exp_3", "exp_en_3", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "exp_4", "exp_en_4", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "participation_challenges", "part_chal_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "exp_5", "exp_en_5", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "feedback_on_team", "fdbk_on_t_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "safety_suggestions", "sfty_sug_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "physical_wellbeing_exp", "phy_wb_exp_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "mental_wellbeing_exp", "mtl_wb_exp_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "financial_wellbeing_exp", "fin_wb_exp_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "family_wellbeing_exp", "fmly_wb_exp_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "recognition_exp", "recgn_exp_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "sense_of_justice_exp", "sns_jus_exp_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "dignity_exp", "dig_exp_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "most_significant_impact", "mst_signf_mpct_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions = translate_dataframe_column(
+    open_questions, "additional_participation_info", "mst_signf_mpct_en", translate_text
+)
+open_questions.head(1)
+# %%
+open_questions.columns
+# %%
+open_questions.to_clipboard()
